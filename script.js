dafe5f106006eab480de9c107a2d570991989c49
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-04-15 13:17:40 +0530

    added script file

--- script.js
+++ script.js
@@ -1,31 +1,29 @@
 const ham = document.querySelector(".hamburger-menu");
-        const headerContent = document.querySelector(".header-content");
-        const closeIcon = document.querySelector(".close");
-        const goTop = document.querySelector('.go-to-top');
-        const nav = document.querySelector("nav")
-        const main = document.querySelector('main-content')
+const headerContent = document.querySelector(".header-content");
+const closeIcon = document.querySelector(".close");
+const goTop = document.querySelector(".go-to-top");
+const nav = document.querySelector("nav");
+const main = document.querySelector("main-content");
 
-        ham.addEventListener("click", (e) => {
-            e.stopPropagation()
-            headerContent.classList.toggle("menu-open")
-        })
+ham.addEventListener("click", (e) => {
+  e.stopPropagation();
+  headerContent.classList.toggle("menu-open");
+});
 
-        nav.addEventListener('click', (e) => {
-            e.stopPropagation();
-        })
+nav.addEventListener("click", (e) => {
+  e.stopPropagation();
+});
 
-        closeIcon.addEventListener("click", (e) => {
-            e.stopPropagation()
-            headerContent.classList.remove("menu-open")
-        })
+closeIcon.addEventListener("click", (e) => {
+  e.stopPropagation();
+  headerContent.classList.remove("menu-open");
+});
 
-        
+// goTop.addEventListener("click", () => {
 
-        // goTop.addEventListener("click", () => {
+//     scrollTo(0, 0);
+// })
 
-        //     scrollTo(0, 0);
-        // })
-
-        goTop.addEventListener("click", () => {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-        });
+goTop.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
