4cfd1244dcc95c2d6fc0f1b6c18550348c08cb53
coder-Rishi05 <rawatrishi1121@gmail.com>
2025-04-15 13:17:09 +0530

    added script file

---
+++ script.js
@@ -0,0 +1,31 @@
+const ham = document.querySelector(".hamburger-menu");
+        const headerContent = document.querySelector(".header-content");
+        const closeIcon = document.querySelector(".close");
+        const goTop = document.querySelector('.go-to-top');
+        const nav = document.querySelector("nav")
+        const main = document.querySelector('main-content')
+
+        ham.addEventListener("click", (e) => {
+            e.stopPropagation()
+            headerContent.classList.toggle("menu-open")
+        })
+
+        nav.addEventListener('click', (e) => {
+            e.stopPropagation();
+        })
+
+        closeIcon.addEventListener("click", (e) => {
+            e.stopPropagation()
+            headerContent.classList.remove("menu-open")
+        })
+
+        
+
+        // goTop.addEventListener("click", () => {
+
+        //     scrollTo(0, 0);
+        // })
+
+        goTop.addEventListener("click", () => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+        });
